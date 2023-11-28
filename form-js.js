7dcf79c25a5c41bf73a3d4ba2f2d7719e48d6199
EricChen0803 <erchen@ucsd.edu>
2023-11-27 18:47:05 -0800

    Commit

--- form-js.js
+++ form-js.js
@@ -16,16 +16,72 @@ document.addEventListener('DOMContentLoaded', function () {
           localStorage.setItem('theme', 'light');
         }
       }
+      function editTheme(){
+        var body = document.body;
+        var currentTheme = 'light';
+        if (body.classList.contains('dark-theme')){
+            currentTheme = 'dark';
+        }
+        var background = document.getElementById('background_picker');
+        var text = document.getElementById('text_picker');
+        var font = document.getElementById('font_picker');
+        if(currentTheme=='light'){
+            document.documentElement.style.setProperty('--text-light', text.value);
+            document.documentElement.style.setProperty('--background-light', background.value);
+            document.documentElement.style.setProperty('--font-light', font.value);
+            localStorage.setItem('text-light', text.value);
+            localStorage.setItem('background-light', background.value);
+            localStorage.setItem('font-light', font.value);
+        }
+        else{
+            document.documentElement.style.setProperty('--text-dark', text.value);
+            document.documentElement.style.setProperty('--background-dark', background.value);
+            document.documentElement.style.setProperty('--font-dark', font.value);
+            localStorage.setItem('text-dark', text.value);
+            localStorage.setItem('background-dark', background.value);
+            localStorage.setItem('font-dark', font.value);
+        }
+      }
       
       var savedTheme = localStorage.getItem('theme');
       if (savedTheme === 'dark') {
         document.body.classList.add('dark-theme');
+      }
+
+      var light_background = localStorage.getItem('background-light');
+      if(light_background!=null){
+        document.documentElement.style.setProperty('--background-light', light_background);
+      }
+      var light_text = localStorage.getItem('text-light');
+      if(light_text!=null){
+        document.documentElement.style.setProperty('--text-light', light_text);
+      }
+      var light_font = localStorage.getItem('font-light');
+      if(light_font!=null){
+        document.documentElement.style.setProperty('--font-light', light_font);
+      }
+      var dark_background = localStorage.getItem('background-dark');
+      if(dark_background!=null){
+        document.documentElement.style.setProperty('--background-dark', dark_background);
+      }
+      var dark_text = localStorage.getItem('text-dark');
+      if(dark_text!=null){
+        document.documentElement.style.setProperty('--text-dark', dark_text);
+      }
+      var dark_font = localStorage.getItem('font-dark');
+      if(dark_font!=null){
+        document.documentElement.style.setProperty('--font-dark', dark_font);
       }
     
     
     var changeThemebtn = document.getElementById('theme');
     if (changeThemebtn) {
         changeThemebtn.addEventListener('click', changeTheme);
+    }
+
+    var editThemebtn = document.getElementById('edit_theme');
+    if (editThemebtn) {
+        editThemebtn.addEventListener('click', editTheme);
     }
     var formErrors = [];
     var name = document.getElementById("name");
@@ -170,4 +226,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 });
-
